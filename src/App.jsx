7dec3f697b6c615c864f5dbf5451415b1bad45c2
TowmirAtInsta <towmir@instawebworks.com.au>
2023-01-05 13:14:04 +0600

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
 
 import { Box, Typography } from "@mui/material";
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
+
+
 
 const ZOHO = window.ZOHO;
 
 function App() {
   const [initialized, setInitialized] = useState(false) // initialize the webtab
   const [targetDeals, setTargetDeals] = useState([]) // keeps the deals
-  const [pageSize, setPageSize] = useState(10)
+  const [pageSize, setPageSize] = useState(10) // datagrid table pagesize
+  const [currentUser, setCurrentUser] = useState("") // owner of the deals
+
 
   useEffect(() => {
     ZOHO.embeddedApp.on("PageLoad", function (data) { // initialize the app
@@ -24,6 +28,7 @@ function App() {
         
         const userResp = await ZOHO.CRM.CONFIG.getCurrentUser() // get the current logged in user
         const current_user = userResp?.users?.[0]?.id;
+        setCurrentUser(userResp?.users?.[0]?.full_name)
   
         const conn_name = "zoho_crm_conn";
         let req_data = {
@@ -38,8 +43,8 @@ function App() {
   
         const dealsResp = await ZOHO.CRM.CONNECTION.invoke(conn_name, req_data) // target deals collected
         console.log(dealsResp?.details?.statusMessage?.data)
-        let sortedDeals = dealsResp?.details?.statusMessage?.data?.sort((deal1, deal2) => (deal1.Amount < deal2.Amount) ? 1 : (deal1.Amount > deal2.Amount) ? -1 : 0)
-        let finalDealsArray = sortedDeals.map((deal, index) => {
+        let sortedDeals = dealsResp?.details?.statusMessage?.data?.sort((deal1, deal2) => (deal1.Amount < deal2.Amount) ? 1 : (deal1.Amount > deal2.Amount) ? -1 : 0) // sorts the row in dsc order based on amount
+        let finalDealsArray = sortedDeals.map((deal, index) => { // adds a custom property to the deals to show the rank of each deal
           return {
             ...deal,
             "rank": index + 1
@@ -51,7 +56,7 @@ function App() {
     fetchData();
   }, [initialized])
 
-  const formatter = new Intl.NumberFormat('en-US', {
+  const formatter = new Intl.NumberFormat('en-US', { // js formatter for currency
     style: 'currency',
     currency: 'USD',
   });
@@ -61,7 +66,7 @@ function App() {
     { 
       field: 'rank', 
       headerName: 'Rank', 
-      flex: 1.5
+      flex: 1
     },
     {
       field: 'Deal_Name',
@@ -69,7 +74,7 @@ function App() {
       renderCell: (params) => (
         <Box>
             <a 
-              href={`https://crm.zoho.com/crm/org651752009/tab/Potentials/${params.row.id}`} 
+              href={`https://crm.zoho.com/crm/org651752009/tab/Potentials/${params.row.id}`} // adds the deal id from the current row data to the custom link
               style={{ 
                 color: "#1976d2",
                 textDecoration: "none"
@@ -98,7 +103,7 @@ function App() {
     {
       field: 'Last_Follow_Up_Date',
       headerName: 'Last Contact Date',
-      flex: 2
+      flex: 2.5
     },
     {
       field: 'Days_From_Now',
@@ -121,11 +126,11 @@ function App() {
         p: "2rem 3rem"
       }}
     >
-      <h2 style={{ marginBottom: "1rem" }}>Deals for Current Logged In User</h2>
+      <h2 style={{ marginBottom: "1rem" }}>Deals for {currentUser}</h2>
       <Box
         sx={{
           width: "100%",
-          height: 640
+          height: `calc(111px + ${pageSize * 40}px)` // changes the height according to the number of rows, default row height is 52px in MUI datagrid
         }}
       >
         <DataGrid
@@ -135,6 +140,7 @@ function App() {
           onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
           rowsPerPageOptions={[5, 10]}
           disableSelectionOnClick
+          rowHeight={40}
         />
       </Box>
     </Box>
